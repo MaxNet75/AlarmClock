ff68d3354bd7439cb72f74e299fce60153c00298
MaxNet75 <maxime.baguelin@gmail.com>
2020-02-24 23:26:55 +0100

    Update alarmClock.py

--- alarmClock.py
+++ alarmClock.py
@@ -501,8 +501,8 @@ bedroom2OffsetDecrease.place(anchor = 'center', x = bedroom2Frame.winfo_width() 
 #
 # EXIT
 #
-buttonExit=tkinter.Button(root, text="X", command=exit)
+buttonExit=tkinter.Button(root, text="Exit", command=exit)
 buttonExit.pack()
-buttonExit.place(anchor="center", x=screenWidth-10, y=10, width=50, height=30)
+buttonExit.place(anchor="center", x=screenWidth-20, y=screenHeigh/2+20, width=50, height=30)
 
 root.mainloop()
