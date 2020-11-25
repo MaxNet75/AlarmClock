b694257fc1bca08c6ea5b1080cd7cdc424ed2154
MaxNet <maxnet@iMac-de-MaxNet.local>
2020-11-25 20:32:55 +0100

    change button

--- alarmClock.py
+++ alarmClock.py
@@ -409,8 +409,8 @@ imageDisabled = tkinter.PhotoImage(file='disabled.png')
 #
 # Bedroom 1
 #
-bedroom1Status = tkinter.Checkbutton(image=imageDisabled, selectimage=imageEnabled, text='Bedroom 1', variable=bedroom1StatusVariable, bg='black', activebackground='black', fg='red', activeforeground='red', font=('arial', 15), bd=0, highlightthickness=0, highlightcolor='black')
-bedroom1Status.pack(pady=20)
+bedroom1Status = tkinter.Checkbutton(image=imageDisabled, selectimage=imageEnabled, text='Bedroom 1', variable=bedroom1StatusVariable, indicatoron='false', bg='black', activebackground='black', fg='red', activeforeground='red', font=('arial', 15), bd=0, highlightthickness=0, highlightcolor='black')
+bedroom1Status.pack(pady=-20)
 
 bedroom1Frame = tkinter.LabelFrame(root, labelwidget=bedroom1Status, bg = 'black', fg = 'red')
 bedroom1Frame.pack()
