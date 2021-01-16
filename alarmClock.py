19c610bc78c7f5c2b41535d31c546c26c2bfb369
MaxNet75 <maxime.baguelin@gmail.com>
2021-01-16 16:54:52 +0100

    Update alarmClock.py

--- alarmClock.py
+++ alarmClock.py
@@ -95,7 +95,7 @@ def decreaseVolume():
 # Clock
 #
 def getClockInMinutes():
-	print time.strftime("%H")
+	print str(time.strftime("%H"))
 	return str(int(time.strftime("%H")) * 60 + int(time.strftime("%M")))
 
 def clockUpdate(label):
