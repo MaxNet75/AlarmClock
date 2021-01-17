f1337b72bdf9386d8c2d975d11e47b15a0e95b42
MaxNet75 <maxime.baguelin@gmail.com>
2021-01-17 19:04:11 +0100

    Update alarmClock.py

--- alarmClock.py
+++ alarmClock.py
@@ -381,7 +381,7 @@ radioFrame.update()
 
 for radioKey, radioValue in radioList.items():
 	imageRadio = tkinter.PhotoImage(file=radioValue['image'])
-	radiobuttonRadio=tkinter.Radiobutton(radioFrame, anchor="center", image=imageRadio, text=radioValue['name'], variable=radioSelect, value=radioKey, bg='black', activeforeground='black', activebackground='black', indicatoron='false')
+	radiobuttonRadio=tkinter.Radiobutton(radioFrame, anchor="center", image=imageRadio, variable=radioSelect, value=radioKey, bg='black', activeforeground='black', activebackground='black', indicatoron='false')
 	radiobuttonRadio.pack()
 	radiobuttonRadio.place(anchor = 'center', x = radioFrame.winfo_width() / (len(radioList) + 1) * int(radioKey), y = radioFrame.winfo_height() / 5, width = 80, height = 80)
 
